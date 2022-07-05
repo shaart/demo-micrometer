0b87d1f99817cc19b03b73760c026c81e8c5866a
Artur Shalaev <imshaart@gmail.com>
2022-07-05 21:13:38 +0300

    TimedAspect with sample CalculationJob added; @Scheduled pool size increased to 2; application.name specified

---
+++ src/main/java/shaart/demo/micrometer/job/CalculationJob.java
@@ -0,0 +1,30 @@
+package shaart.demo.micrometer.job;
+
+import io.micrometer.core.annotation.Timed;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+public class CalculationJob {
+
+  @Timed(value = "sample.calculations", description = "Time taken to do sample calculations")
+  @Scheduled(fixedDelay = 60, timeUnit = TimeUnit.SECONDS)
+  public void calcJob() {
+    log.info("## Calc job start");
+    long sum = 0;
+    for (int i = 0; i < 1_000_000; i++) {
+      sum += ThreadLocalRandom.current().nextInt(0, 1000);
+
+      if (i % 10_000 == 0) {
+        log.info("#calc{}", i);
+      }
+    }
+    log.info("Sum: {}", sum);
+    log.info("## Calc job end");
+  }
+}
